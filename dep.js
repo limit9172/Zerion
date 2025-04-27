e8d2e3372d2bf00420d54f90828897c7fa2371c3
Loard <zaidangaming47@gmail.com>
2025-04-27 22:18:39 +0700

    Update dep.js

--- dep.js
+++ dep.js
@@ -5,8 +5,7 @@ const token = '7837750888:AAGB6UF0DsqFZWGzwwXyGrrikg5gRPZ9PSg';
 const bot = new TelegramBot(token);
 
 // Setup webhook URL di Vercel
-const url = `https://<your-vercel-deployment-url>/api/webhook`;  // Ganti dengan URL deployment Vercel
-
+const url = `https://vercel.com/loards-projects/zerion/CvmsoCpbmfM9ZSfkmTefUAZ6EY8A/api/webhook`;
 // Set webhook ke Telegram bot
 bot.setWebHook(url);
 
